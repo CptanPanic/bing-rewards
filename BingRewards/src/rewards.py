6933ba15488ff0c07eb16676f04baf5ccb415c7a
CptanPanic <brian@forceconstant.com>
2020-10-07 09:31:38 -0400

    add total

--- BingRewards/src/rewards.py
+++ BingRewards/src/rewards.py
@@ -845,7 +845,7 @@ class Rewards:
                 self.__sys_out("Available points: " + stats[avail_index].text, 2)
 				
                 if len(Client().user_key) > 0 :
-                    Client().send_message("Points earned: " + stats[earned_index].text.replace(" ", ""), title="BingRewards Completed", sound="none")
+                    Client().send_message("Points earned: " + stats[earned_index].text.replace(" ", "") + " Available points: " + stats[avail_index].text, title="BingRewards Completed", sound="none")
 
             else:
                 self.__sys_out("Summary", 1, flush=True)
@@ -855,7 +855,7 @@ class Rewards:
                 self.__sys_out("Available points: " + stats[avail_index+1].text, 2)
                 
                 if len(Client().user_key) > 0 :
-                    Client().send_message("Points earned: " + stats[earned_index+1].text.replace(" ", ""), title="BingRewards Completed", sound="none")
+                    Client().send_message("Points earned: " + stats[earned_index+1].text.replace(" ", "") + " Available points: " + stats[avail_index].text, title="BingRewards Completed", sound="none")
 
         except Exception as e:
             print('    Error checking rewards status - ', e)
